9439a2796454bd1fa02d3ee2f07cca69985ebb66
Mokhtar Hussein Abdulwahab Alsorori <tarasana4const@gmail.com>
2026-08-22 22:11:13 +0300

    fix(release): remove unused legal screen import

--- lib/features/profile/presentation/legal_and_deletion_screens.dart
+++ lib/features/profile/presentation/legal_and_deletion_screens.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-import '../../../core/config/app_config.dart';
 import '../../../core/theme/app_theme.dart';
 import '../../../providers/app_providers.dart';
 import '../../notifications/presentation/fcm_token_service.dart';
